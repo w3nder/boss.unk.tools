e1811b6d95dee12ae86ca7a8671b3025c080fc43
Wender Teixeira <unktools@gmail.com>
2022-02-01 14:01:57 -0300

    fix path image

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -138,7 +138,7 @@ export default function MediaCard(props) {
                 <CardMedia
                   component="img"
                   height="140"
-                  image={`http://localhost:4001/${token.image}`}
+                  image={`https://status-boss.herokuapp.com/${token.image}`}
                   alt={token.nome}
                 />
                 <CardContent
